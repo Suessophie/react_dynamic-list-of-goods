24dab1c62b43d61115b9f48a095e29d35f60b6a6
Sofiia Kucherenko <kucherenko.sv13@gmail.com>
2020-11-04 19:16:36 +0300

    Updates

---
+++ precache-manifest.e5fb822886d2367d796ecb85ef6ba3cd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f68d7063dab9fb0cae47483a6d7fc299",
+    "url": "./index.html"
+  },
+  {
+    "revision": "9f0b5c07f2b9c6dbb1a2",
+    "url": "./static/css/main.819e4dd6.chunk.css"
+  },
+  {
+    "revision": "56520c5f223d9c08cda6",
+    "url": "./static/js/2.3d236229.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.3d236229.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f0b5c07f2b9c6dbb1a2",
+    "url": "./static/js/main.daf17904.chunk.js"
+  },
+  {
+    "revision": "cc2a5e26d5c4844758e0",
+    "url": "./static/js/runtime-main.beb1984a.js"
+  }
+]);
